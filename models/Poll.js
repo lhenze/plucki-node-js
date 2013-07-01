4d60418c62da607be8beb70497b97ad5f01973b2
Laura Henze <laurahenze@gmail.com>
2013-07-01 19:33:48 -0400

    cleaning up averaging function

--- models/Poll.js
+++ models/Poll.js
@@ -18,7 +18,6 @@ var PollSchema = Schema({
 });
 
 PollSchema.statics.averageResults = function(id, cb) {
-	// this.find({ name: new RegExp(name, 'i') }, cb);
 	this.findById(id).populate('users', 'items').exec(function(err, thisPoll) {
 		if (err) {
 			console.log("static: " + err);
@@ -28,21 +27,18 @@ PollSchema.statics.averageResults = function(id, cb) {
 			var newCleanArray = __.filter(thisPoll.users, function(userObj) {
 				return ((typeof userObj === 'undefined') || (typeof userObj.items === 'undefined') || (userObj.items.length == 0 ));
 			});
-			//console.log("newCleanArray is " + newCleanArray.length);
+			
 			thisPoll.users = newCleanArray;
-			//console.log("thisPoll.users " + thisPoll.users);
 			for (var i = 0; i < thisPoll.items.length; i++) {
 				console.log(" --> it is " + thisPoll.items[i].url);
 				var sum = 0;
 				var avg = 0;
 				for (var r = 0; r < thisPoll.users.length; r++) {
-					//console.log("** testing " + thisPoll.users[r].items);
-					//console.log("it is " + thisPoll.users[r].username + " " + thisPoll.users[r].items + " " + thisPoll.users[r].items[i].score);
 					// Be careful - this will only work if the items appear in the same order
+					// This should be fixed to reference by items' urls
 					sum += parseInt(thisPoll.users[r].items[i].score, 10);
 				}
 				avg = parseInt(sum / thisPoll.users.length, 10);
-				//console.log("avg! " + avg);
 				thisPoll.items[i].score = avg;
 
 			}
